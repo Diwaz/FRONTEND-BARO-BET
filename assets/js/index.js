5fcc61eb3fa6045acd0ab7ce8896ac58c68cc0fc
diwaz <diwaswatson@gmail.com>
2023-03-18 13:41:54 +0545

    almost readt homepage version

--- assets/js/index.js
+++ assets/js/index.js
@@ -37,3 +37,27 @@ $(".tab a").on("click", function(e) {
 
     $(target).fadeIn(600);
 });
+
+const registerContent = document.querySelector('.signUpDiv');
+const afterRegisterContent = document.querySelector('#afterSignUp');
+const registerButton = document.querySelector('#registerBtn');
+
+// Check if the registration state is saved in local storage
+const registrationState = localStorage.getItem('registrationState');
+if (registrationState === 'after') {
+    // Show the after-registration component
+    registerContent.style.display = 'none';
+    afterRegisterContent.style.display = 'block';
+}
+
+// Bind a click event handler to the register button
+registerButton.addEventListener('click', function() {
+    // Hide the registration component
+    registerContent.style.display = 'none';
+
+    // Show the after-registration component
+    afterRegisterContent.style.display = 'block';
+
+    // Save the registration state to local storage
+    localStorage.setItem('registrationState', 'after');
+});
