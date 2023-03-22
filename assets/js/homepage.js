d326b71d3445492536b62d6b07c869b96eae8264
diwaz <diwaswatson@gmail.com>
2023-03-22 18:57:00 +0545

    basic responsive homepage
    last step of before building logic of homepage

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,44 @@ const triggerSidebar = document.querySelector('.tooglebarSegment');
 const sidebar = document.querySelector('.sidebar');
 const backdrop = document.querySelector('.my-backdrop');
 
+
+
+
+
+
+const gameData = {
+
+    League1: {
+        leagueFlag: 'Spain',
+        leagueName: 'la Liga',
+        Game1: {
+            team1: {
+                date: "03/11",
+                logo: "assets/images/team1.png",
+                name: "Arsenal FC"
+            },
+            team2: {
+                time: "6:00",
+                logo: "assets/images/team2.png",
+                name: "Bayern FC"
+            },
+            odds: {
+                bet: ["3.50", "3.20", "3.00"],
+                handi: [
+                    { label: "-1.5", value: "2.10" },
+                    { label: "1.5", value: "1.64" }
+                ],
+                extra: "+78"
+            }
+        }
+    }
+
+};
+
+
+
+
+
 function moveElement() {
     sidebar.style.left = '0';
     console.log('clicked')
